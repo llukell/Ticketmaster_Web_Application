2185e0b6216cd6b30bd452e78857ad5f59238ad5
llukell <lukelapila@outlook.com>
2023-12-13 18:46:28 -0500

    updated settings

--- ticketmaster1/settings.py
+++ ticketmaster1/settings.py
@@ -113,10 +113,10 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'static'
